a4871fadcc1c8962793ba20f21b3448314a1df4a
muksis <60219890+muksis@users.noreply.github.com>
2023-03-28 13:05:00 -0400

    Add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { Container, Box } from '@chakra-ui/react';
+import Header from './components/Header';
 
 const App = () => {
   return (
     <Box bg='blue.600' color='white' height='100vh' paddingTop={130}>
       <Container maxW='3xl' centerContent>
-        <h1>My App</h1>
+        <Header />
       </Container>
     </Box>
   );
