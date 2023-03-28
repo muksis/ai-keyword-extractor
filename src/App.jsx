6db4e45589df96c1fbcde46e2d9032531ac398bb
muksis <60219890+muksis@users.noreply.github.com>
2023-03-28 14:36:06 -0400

    Add keywords modal

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Container, Box } from '@chakra-ui/react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import TextInput from './components/TextInput';
+import KeywordsModal from './components/KeywordsModal';
 
 const App = () => {
   const [keywords, setKeywords] = useState('');
@@ -20,7 +21,7 @@ const App = () => {
         Authorization: `Bearer ${import.meta.env.VITE_OPENAI_API_KEY}`,
       },
       body: JSON.stringify({
-        model: 'text-davinchi-003',
+        model: 'text-davinci-003',
         prompt:
           'Extract keywords from this text. Make the first letter of each word uppercase and separate with commas\n\n' +
           text +
@@ -42,6 +43,10 @@ const App = () => {
     setLoading(false);
   };
 
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
   return (
     <Box bg='blue.600' color='white' height='100vh' paddingTop={130}>
       <Container maxW='3xl' centerContent>
@@ -49,6 +54,12 @@ const App = () => {
         <TextInput extractKeywords={extractKeywords} />
         <Footer />
       </Container>
+      <KeywordsModal
+        keywords={keywords}
+        loading={loading}
+        isOpen={isOpen}
+        closeModal={closeModal}
+      />
     </Box>
   );
 };
