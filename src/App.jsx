5e0de02c84586112b83bd75b958c73a4a1bc1d73
muksis <60219890+muksis@users.noreply.github.com>
2023-03-28 12:57:19 -0400

    Add basic setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import { Container, Box } from '@chakra-ui/react';
+
 const App = () => {
   return (
-    <>
-      <h1>My App</h1>
-    </>
+    <Box bg='blue.600' color='white' height='100vh' paddingTop={130}>
+      <Container maxW='3xl' centerContent>
+        <h1>My App</h1>
+      </Container>
+    </Box>
   );
 };
 
